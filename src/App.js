e964afe0c949fab7679ca94973ce0a3f364690a4
Matt Sirkis <sirkis.matt@gmail.com>
2020-08-21 18:05:21 +1200

    added callback props for search box and filterOoptions

--- src/App.js
+++ src/App.js
@@ -11,11 +11,33 @@ class App extends Component {
       filterOption: 'All'
     };
   }
+
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term
+    })
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar/>
-        <FilterableList files={this.props.files} />
+        <SearchBar
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption}
+        handleUpdate={term=>this.updateSearchTerm(term)}
+        handleFilterChange={option => this.updateFilterOption(option)}
+        />
+        <FilterableList 
+        files={this.props.files} 
+        searchTerm={this.state.searchTerm}
+        filterOption={this.state.filterOption}
+        />
       </div>
     );
   }
